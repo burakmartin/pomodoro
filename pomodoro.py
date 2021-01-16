510a1d2475fb035250d2d0908690b80a8afd43ef
burakmartin <burak.martin@mail.de>
2021-01-16 02:32:45 +0100

    changed license

--- pomodoro.py
+++ pomodoro.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
 
+# Pomodoro is a small GUI for battling procrastination.
+# Copyright (C) 2021  Burak Martin
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
 from PyQt5.QtCore import QTime, QTimer, Qt
 from PyQt5.QtWidgets import QSizePolicy, QWidget, QMainWindow, QVBoxLayout, QGroupBox, QHBoxLayout, QSpinBox, QLabel,\
                             QComboBox, QLCDNumber, QPushButton, QSystemTrayIcon, QMenu, QApplication, QTabWidget,\
